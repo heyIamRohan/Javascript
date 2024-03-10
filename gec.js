ec3ca995c9eca2807cd9a97dad88d60a5c2ad326
Rohan Kumar <152252631+heyIamRohan@users.noreply.github.com>
2024-03-10 23:25:32 +0530

    Add files via upload

---
+++ gec.js
@@ -0,0 +1,14 @@
+// var a = "hello";
+// console.log(a);
+// var b = "hii";
+// console.log(b);
+/////////
+// let a = 10;
+// let b = 20;
+// var c =30;
+// console.log(c);
+////////////////
+var a = 100;
+let c = 200;
+console.log(c);
+console.log(a);
